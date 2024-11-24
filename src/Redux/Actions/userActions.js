b820f73630b35430e1a02a6fa56184a840965eef
duclong1311 <duclong1311.work@gmail.com>
2024-11-25 02:02:07 +0700

    add actions fetch dât

--- src/Redux/Actions/userActions.js
+++ src/Redux/Actions/userActions.js
@@ -1,5 +1,8 @@
 export const FETCH_USER_LOGIN_SUCCESS = 'FETCH_USER_LOGIN_SUCCESS';
 export const USER_LOGOUT_SUCCESS = 'USER_LOGOUT_SUCCESS';
+export const FETCH_USER_REQUEST = 'FETCH_USER_REQUEST';
+export const FETCH_USER_SUCESS = 'FETCH_USER_SUCESS';
+export const FETCH_USER_ERROR = 'FETCH_USER_ERROR';
 
 export const doLogin = (data) => {
     return {
@@ -11,5 +14,23 @@ export const doLogin = (data) => {
 export const doLogout = () => {
     return {
         type: USER_LOGOUT_SUCCESS,
+    }
+}
+
+export const fetchUsersRequest = () => {
+    return {
+        type: FETCH_USER_REQUEST
+    }
+}
+
+export const fetchUserSucess = (payload) => {
+    return {
+        type: FETCH_USER_SUCESS
+    }
+}
+
+export const fetchUserError = (payload) => {
+    return {
+        type: FETCH_USER_ERROR
     }
 }
